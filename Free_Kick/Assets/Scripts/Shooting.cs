8301d5f60331a2479cb8e9693a081f023fbce4f6
Phyzon21 <jjRabino@mcm.edu.ph>
2021-03-23 21:12:37 +0800

    Added Shooting Script

---
+++ Free_Kick/Assets/Scripts/Shooting.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+
+    public GameObject ball;
+
+    public float ballDistance = 2f;
+    public float ballThrowingForce = 5f;
+
+    Vector3 shootingArc;
+
+    private int randomIntZ;
+    private int randomIntY;
+
+    private bool holdingBall = true;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.GetComponent<Rigidbody>().useGravity = false;
+
+        randomIntZ = Random.Range(50, 100);
+        randomIntY = Random.Range(100, 150);
+        shootingArc = new Vector3(0, randomIntY, randomIntZ);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (holdingBall)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ball.GetComponent<Rigidbody>().useGravity = true;
+                ball.GetComponent<Rigidbody>().AddForce(shootingArc.normalized * ballThrowingForce);
+                holdingBall = false;
+            }
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Checker")
+        {
+            Debug.Log("SCORE!!");
+        }
+    }
+}
